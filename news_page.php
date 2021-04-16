bbe4dbc26e768a1f6dc8a61bfcb7307ed4ab636c
FrutlesPoent <frutlespoent@gmail.com>
2021-04-17 00:19:48 +0700

    Add full page of news

---
+++ news_page.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RelaxNews</title>
+    <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="css/cs2.css">
+    <link rel="stylesheet" href="css/css_news/css_news.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body style="background-color:rgb(40, 38, 38)">  
+    <div class="wrapper">
+        <div class="section">
+                <header>
+                    <span class="header_phrase_right" >Мирные новости на RelaxNews</span>
+                    <a href="index.php"><div class="header_phrase_one">Relax</div></a>
+                    <a href="index.php"><div class="header_phrase_two">News</div></a>
+                    <div class="header_phrase_news">Новости</div>
+                    <div class="header_phrase_todolist"><a href="https://github.com/RelaxToDoList/ToDo/releases/tag/v1.0"><p class="mycolor">Relax Todo list</p></a></div>
+                </header>
+            </div>
+        </div>
+    </div>
+    
+    <div class="container">
+  <div class="row">
+    <div class="col-md-8">
+    <div id="news">
+      <div class="wp-block property list">
+        <div class="wp-block-body">
+          <div class="wp-block-img">
+            <a href="news_page.php">
+              <img src="https://i.artfile.ru/2048x1365_909032_[www.ArtFile.ru].jpg" alt="">
+            </a>
+          </div>
+          <div class="wp-block-content">
+            <small>
+          <span class="glyphicon glyphicon-calendar" aria-hidden="true"></span> 16.04.2021</small>
+            <h4 class="content-title">Lorem ipsum</h4>
+            <p class="description">Lorem ipsum dolor sit amet, consectetur adipisg elitm Ut tincidunt purus iaculis ipsum dctum non dtum quam.Lorem ipsum dolor sit amet, consectetur adipisg elitm Ut tincidunt purus iaculis ipsum dctum non dtum quam.Lorem ipsum dolor sit amet, consectetur adipisg elitm Ut tincidunt purus iaculis ipsum dctum non dtum quam.Lorem ipsum dolor sit amet, consectetur adipisg elitm Ut tincidunt purus iaculis ipsum dctum non dtum quam.Lorem ipsum dolor sit amet, consectetur adipisg elitm Ut tincidunt purus iaculis ipsum dctum non dtum quam.</p>
+            <span class="pull-left">
+              <span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span>
+            </span>
+            <span class="pull-right">
+              <span class="capacity">
+                <i class="fa fa-user"></i> <span class="nick-name">Имя Фамилия</span>
+              </span>
+            </span>
+          </div>
+        </div>
+      </div>
+  </div> 
+  </div>
+</div>
+    
+
+<script src="/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+</body>
+</html>
