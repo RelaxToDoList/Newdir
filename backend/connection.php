b6c0fd3353dc74ebe82ca01a0b458aa5cdca33db
FrutlesPoent <frutlespoent@gmail.com>
2021-04-17 16:30:23 +0700

    something

--- backend/connection.php
+++ backend/connection.php
@@ -2,5 +2,5 @@
 $host = 'localhost'; // адрес сервера 
 $database = 'data_base'; // имя базы данных
 $user = 'root'; // имя пользователя
-$password = ''; // пароль
+$password = 'root'; // пароль
 ?>
